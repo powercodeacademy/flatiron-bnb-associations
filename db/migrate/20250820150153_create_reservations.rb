554ac67cf212b0339ca8e5674026935b9658a59c
Emily Kanarek <emilyakanarek@gmail.com>
2025-08-20 11:04:10 -0400

    Create tables

---
+++ db/migrate/20250820150153_create_reservations.rb
@@ -0,0 +1,12 @@
+class CreateReservations < ActiveRecord::Migration[7.1]
+  def change
+    create_table :reservations do |t|
+      t.date :checkin
+      t.date :checkout
+      t.integer :listing_id
+      t.integer :guest_id
+
+      t.timestamps
+    end
+  end
+end
